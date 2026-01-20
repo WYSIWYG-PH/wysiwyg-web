720a076746d76b219f02961d766f2e76dc395035
ErmanDev <ermanfaminiano020@gmail.com>
2026-01-20 10:43:42 +0800

    convert the static year to dynamic

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,12 @@
 <template>
     <footer class="bg-slate-950 text-white py-10 px-6 flex justify-between items-center border-t-2 border-primary">
-        <span class="text-sm">© 2025 DevBiz. All rights reserved.</span>
+        <span class="text-sm">© {{ currentYear }} DevBiz. All rights reserved.</span>
 
     </footer>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const currentYear = computed(() => new Date().getFullYear())
+</script>
